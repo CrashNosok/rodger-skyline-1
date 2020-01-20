e9cbdc524acbddebf4c2ac36411a0ca543e2f247
amada <amada@debian.amada>
2020-01-20 14:19:34 -0500

    all here

---
+++ make_site.sh
@@ -0,0 +1,9 @@
+sudo rm /var/www/html/index.nginx-debian.html
+sudo mkdir /var/www/html/img
+sudo mkdir /var/www/html/css
+sudo mkdir /var/www/html/js
+sudo cp ./html/index.html /var/www/html
+sudo cp ./css/style.css /var/www/html/css/
+sudo cp ./js/game.js /var/www/html/js/
+sudo cp ./img/food.png /var/www/html/img/
+sudo cp ./img/ground.png /var/www/html/img/
